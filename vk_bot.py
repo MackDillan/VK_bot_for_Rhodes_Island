a56fcad586fbf27ab2d6687ad43203e82ef84850
MackDillan <mack123456789@mail.ru>
2020-05-18 22:00:09 +0300

    Initialization
    Initialization

---
+++ vk_bot.py
@@ -0,0 +1,17 @@
+from vk_api.longpoll import VkLongPoll, VkEventType
+import requests
+import vk_api
+import random
+vk_session = vk_api.VkApi(token='32319c102c6f7a48f705c4697eb8c886ce62986248f5a15eb9c702a742296a7cedbcb7c53327860707d68')
+
+longpoll = VkLongPoll(vk_session)
+vk = vk_session.get_api()
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+        if event.text == 'Амия, калькулятор':
+            if event.from_user:
+                vk.messages.send(
+                    user_id=event.user_id,
+                    message='Доктор, держи\nhttps://aceship.github.io/AN-EN-Tags/aklevel.html',
+                    random_id = random.randint(1000000,9999999)
+		)
